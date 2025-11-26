96e988af0d617e41add3c1ccd15830f82f269ab0
triautami12 <triautami120305@gmail.com>
2025-11-27 06:01:56 +0700

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,20 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: "/aplikasi-lapor-fasilkom/",   // <--- tambahkan ini
+
       server: {
         port: 3000,
         host: '0.0.0.0',
       },
+
       plugins: [react()],
+
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
+
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
